9769cc706deb1eb89936dc622d9f772972beabea
Kaushal Patel <kaushalpatel922@gmail.com>
2017-05-02 15:28:25 -0400

    starting files for Notes app

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('starting app');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt', 'Hello '+ user.username + '!', function(err) {
+  if(err) {
+    console.log('unable to write the file');
+  }
+});
