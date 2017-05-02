9e1d5af0063ba4bd5898e714f295546dfb439097
Kaushal Patel <kaushalpatel922@gmail.com>
2017-05-02 16:27:55 -0400

    adding argv

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 console.log('starting app');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-console.log(_.isString(true));
-console.log(_.isString('Kaushal'));
+var command = process.argv[2];
+console.log('Command', command);
 
-// var res = notes.add(1,2);
-// console.log('Result:',res);
-
-// var user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
-//   if(err) {
-//     console.log('unable to write the file');
-//   }
-// });
+if(command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading all notes');
+} else if (command === 'remove') {
+  console.log('Removing note');
+} else {
+  console.log('Command Not Recognized');
+}
--- notes.js
+++ notes.js
@@ -1,6 +1,1 @@
 console.log("Loading Note.js file..");
-
-module.exports.add = (a,b) => {
-  console.log('add');
-  return a+b;
-};
