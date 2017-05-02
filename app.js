afbab47d6cbc4261aa45aed4089fec0e7c0e629d
Kaushal Patel <kaushalpatel922@gmail.com>
2017-05-02 15:56:19 -0400

    adding package.json

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ console.log('starting app');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+console.log(_.isString(true));
+console.log(_.isString('Kaushal'));
 
-fs.appendFile('greetings.txt', 'Hello '+ user.username + '!', function(err) {
-  if(err) {
-    console.log('unable to write the file');
-  }
-});
+// var res = notes.add(1,2);
+// console.log('Result:',res);
+
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
+//   if(err) {
+//     console.log('unable to write the file');
+//   }
+// });
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log("Loading Note.js file..");
+
+module.exports.add = (a,b) => {
+  console.log('add');
+  return a+b;
+};
