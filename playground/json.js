d1488f3a9a711197efa73bf5d2adee9df86b2457
Kaushal Patel <kaushalpatel922@gmail.com>
2017-05-03 16:40:24 -0400

    adding to file

---
+++ playground/json.js
@@ -0,0 +1,31 @@
+// var obj = {
+//   name: 'Kaushal'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+//
+// console.log(typeof stringObj);
+// console.log(stringObj);
+//
+// var personString = '{"name": "Kaushal", "age": 27}';
+// var person = JSON.parse(personString);
+//
+// console.log(person);
+// console.log(typeof person);
+
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'some title',
+  body: 'some body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
