d34fd43af94537107485c59aff5eb8882d76e831
Kaushal Patel <kaushalpatel922@gmail.com>
2017-05-03 16:04:42 -0400

    yargs & functions working

--- notes.js
+++ notes.js
@@ -1,1 +1,24 @@
 console.log("Loading Note.js file..");
+
+var addNote = (title, body) => {
+  console.log('Adding Note', title, body);
+};
+
+var getAll = () => {
+  console.log('Showing All notes');
+};
+
+var getNote = (title) => {
+  console.log('Getting Note', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing Note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
