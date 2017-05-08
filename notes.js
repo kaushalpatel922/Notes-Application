4f8ebe1fd0f36684e5f1851e0f49d71ed253901f
Kaushal Patel <kaushalpatel922@gmail.com>
2017-05-08 13:25:25 -0400

    adding single notes

--- notes.js
+++ notes.js
@@ -1,7 +1,27 @@
 console.log("Loading Note.js file..");
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('Adding Note', title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+    console.log();
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if(duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
